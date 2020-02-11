519ac950db7d1fd0728a997656366ca43e9239ef
cr618 <cyprien.roche18@imperial.ac.uk>
2020-02-11 19:23:45 +0000

    update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     java
     kotlin("jvm") version "1.3.61"
+    antlr
 }
 
 group = "Roche"
@@ -17,6 +18,7 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    antlr("org.antlr:antlr4:4.8")
     testImplementation("junit:junit:4.12")
 }
 
@@ -27,8 +29,14 @@ val compileKotlin: KotlinCompile by tasks
 val compileTestKotlin: KotlinCompile by tasks
 
 compileKotlin.kotlinOptions {
-    jvmTarget = "1.11"
+    jvmTarget = "11"
 }
 compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.11"
+    jvmTarget = "11"
 }
+
+// clean {
+//     doFirst {
+//         delete "${rootDir}/src/main/java/wacc/antlr"
+//     }
+// }
