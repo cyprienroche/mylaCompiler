87b3b5175f61d7fc217deb0707986a6311a8e97d
cr618 <cyprien.roche18@imperial.ac.uk>
2020-02-11 19:50:04 +0000

    print parse tree generated from MylaCode.myla file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,19 @@
+import org.antlr.v4.runtime.CharStreams
+import org.antlr.v4.runtime.CommonTokenStream
+import java.io.File
+
 fun main() {
-    print("Myla compiler")
+    buildAST("src/main/resources/MylaCode.myla")
 }
+
+fun buildAST(path: String) {
+    val input = fileToString(path)
+    val stream = CharStreams.fromString(input)
+    val lexer = MylaLexer(stream)
+    val tokens = CommonTokenStream(lexer)
+    val parser = MylaParser(tokens)
+    val tree = parser.prog()
+    println(tree.toStringTree(parser))
+}
+
+fun fileToString(fileName: String): String = File(fileName).readText(Charsets.UTF_8)
