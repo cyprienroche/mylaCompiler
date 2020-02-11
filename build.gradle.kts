6d89a1912ccb4fdb13dbbfe8319028612aae82f6
cr618 <cyprien.roche18@imperial.ac.uk>
2020-02-11 19:04:24 +0000

    change build.gradle and settings.gradle to .kts

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    java
+    kotlin("jvm") version "1.3.61"
+}
+
+group = "Roche"
+version = "1.0-SNAPSHOT"
+description = """Myla Compiler"""
+
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("junit:junit:4.12")
+}
+
+val test: Test by tasks
+test.testLogging.setEvents(setOf("PASSED", "FAILED", "SKIPPED"))
+
+val compileKotlin: KotlinCompile by tasks
+val compileTestKotlin: KotlinCompile by tasks
+
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.11"
+}
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.11"
+}
