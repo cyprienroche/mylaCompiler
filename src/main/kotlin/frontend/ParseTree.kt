a9f242d11dc503f5ac6a29f26e0d902ffec747f9
cr618 <cyprien.roche18@imperial.ac.uk>
2020-02-15 18:22:36 +0000

    add error listener to parse tree and print errors

--- src/main/kotlin/frontend/ParseTree.kt
+++ src/main/kotlin/frontend/ParseTree.kt
@@ -2,16 +2,25 @@ package frontend
 
 import generated.MylaLexer
 import generated.MylaParser
+import org.antlr.v4.runtime.BaseErrorListener
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
 import org.antlr.v4.runtime.tree.ParseTree
 
-class ParseTree(path: String) {
+class ParseTree(path: String, lexerListener: BaseErrorListener, parserListener: BaseErrorListener) {
     private val lexer = MylaLexer(CharStreams.fromFileName(path))
     private val tokens = CommonTokenStream(lexer)
     private val parser = MylaParser(tokens)
 
-    fun getTree(): ParseTree = parser.prog()
+    init {
+        lexer.removeErrorListeners()
+        lexer.addErrorListener(lexerListener)
 
-    override fun toString(): String = getTree().toStringTree(parser)
+        parser.removeErrorListeners()
+        parser.addErrorListener(parserListener)
+    }
+
+    fun getParseTree(): ParseTree = parser.prog()
+
+    override fun toString(): String = getParseTree().toStringTree(parser)
 }
