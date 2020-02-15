bf4b14bd4c343bdbdbb797b345662093895ff72f
cr618 <cyprien.roche18@imperial.ac.uk>
2020-02-15 18:22:36 +0000

    add error listener to parse tree and print errors

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,19 @@
 import frontend.ParseTree
+import frontend.errors.MylaErrorListener
 
 fun main() {
-    val parseTree = ParseTree(path = "src/main/resources/Example.myla")
+    val path = "src/main/resources/Example.myla"
+    val lexerListener = MylaErrorListener()
+    val parserListener = MylaErrorListener()
+
+    val parseTree = ParseTree(path, lexerListener, parserListener)
+    val tree = parseTree.getParseTree()
+
+    if (lexerListener.errors.isNotEmpty() || parserListener.errors.isNotEmpty()) {
+        lexerListener.errors.forEach { println(it) }
+        parserListener.errors.forEach { println(it) }
+        println("${parserListener.errors.size} parser error(s) detected, no further compilation attempted.")
+        return
+    }
     println(parseTree.toString())
 }
