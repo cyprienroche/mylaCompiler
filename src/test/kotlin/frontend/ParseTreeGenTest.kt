3e1c06a65ed7897fa3fdb801754673aa8f59e337
cr618 <cyprien.roche18@imperial.ac.uk>
2020-02-15 20:16:42 +0000

    add test to test malformed programs

--- src/test/kotlin/frontend/ParseTreeGenTest.kt
+++ src/test/kotlin/frontend/ParseTreeGenTest.kt
@@ -1,6 +1,9 @@
 package frontend
 
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.eq
 import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
 import org.antlr.v4.runtime.ANTLRErrorListener
 import org.hamcrest.core.Is.`is`
 import org.junit.Assert.assertThat
@@ -20,5 +23,21 @@ class ParseTreeGenTest {
     fun getCorrectParseTreeForIntegerAssignment() {
         val parseTree = ParseTreeGen("src/test/resources/assign.myla", listener)
         assertThat(parseTree.parseTreeString(), `is`("(prog (stat (identifier x) = (expr (literal 73))) <EOF>)"))
+    }
+
+    @Test
+    fun wrong() {
+        val parseTree = ParseTreeGen(
+            "src/test/resources/invlid.myla", listener
+        )
+        parseTree.parseTreeString()
+        verify(listener).syntaxError(
+            any(),
+            any(),
+            eq(1),
+            eq(5),
+            eq("mismatched input '<EOF>' expecting {NAT, '-', '+', '('}"),
+            any()
+        )
     }
 }
