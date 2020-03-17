0719343fe52654760212741ab870fb56f951fece
Cyprien <roche.cyprien@gmail.com>
2020-03-17 19:19:43 +0100

    [error] MockParser add verifyValid and Invalid fun and parse method return this

--- src/test/kotlin/frontend/mock/MockParser.kt
+++ src/test/kotlin/frontend/mock/MockParser.kt
@@ -1,55 +1,66 @@
 package frontend.mock
 
+import com.nhaarman.mockitokotlin2.atLeastOnce
 import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.never
 import generated.MylaLexer
 import generated.MylaParser
 import org.antlr.v4.runtime.ANTLRErrorListener
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
 
-class MockParser(input: String) {
-    val listener = mock<ANTLRErrorListener> {}
+class MockParser(private val input: String) {
+    private val listener = mock<ANTLRErrorListener> {}
     private val parser: MylaParser
 
     init {
         val stream = CharStreams.fromString(input)
         val lexer = MylaLexer(stream)
+        val tokens = CommonTokenStream(lexer)
+        parser = MylaParser(tokens)
 
         lexer.removeErrorListeners()
         lexer.addErrorListener(listener)
 
-        val tokens = CommonTokenStream(lexer)
-        val parser = MylaParser(tokens)
-
         parser.removeErrorListeners()
         parser.addErrorListener(listener)
-
-        this.parser = parser
     }
 
-    fun parseStatement() {
+    fun parseStatement(): MockParser {
         parser.stat()
         errorIfEntireInputNotParsed()
+        return this
     }
 
-    fun parseAssignLHS() {
+    fun parseAssignLHS(): MockParser {
         parser.assignLHS()
         errorIfEntireInputNotParsed()
+        return this
     }
 
-    fun parseExpression() {
+    fun parseExpression(): MockParser {
         parser.expr()
         errorIfEntireInputNotParsed()
+        return this
     }
 
-    fun parseLiteral() {
+    fun parseLiteral(): MockParser {
         parser.literal()
         errorIfEntireInputNotParsed()
+        return this
     }
 
     private fun errorIfEntireInputNotParsed() {
         if (parser.currentToken.text != "<EOF>") {
             listener.syntaxError(mock {}, mock {}, 0, 0, "", mock {})
         }
+    }
+
+    fun verifyValid() {
+        SyntaxError(listener, never().description("A syntax error occurred when parsing $input")).verify()
+    }
+
+    fun verifyInvalid() {
+        SyntaxError(listener, atLeastOnce().description("The parser could not catch any errors in $input")).verify()
     }
 }
