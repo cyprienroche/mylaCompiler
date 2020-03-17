15e3ad663de6e5b1f7857374323ebe309248e897
Cyprien <roche.cyprien@gmail.com>
2020-03-17 19:18:33 +0100

    [error] SyntaxError make every method return this

--- src/test/kotlin/frontend/mock/SyntaxError.kt
+++ src/test/kotlin/frontend/mock/SyntaxError.kt
@@ -17,28 +17,34 @@ class SyntaxError(mock: ANTLRErrorListener, mode: VerificationMode) {
     private var msg: String = anyOrNull()
     private var e: RecognitionException = anyOrNull()
 
-    fun withRecognizer(recognizer: Recognizer<*, *>) {
+    fun withRecognizer(recognizer: Recognizer<*, *>): SyntaxError {
         this.recognizer = recognizer
+        return this
     }
 
-    fun withOffendingSymbol(offendingSymbol: Any) {
+    fun withOffendingSymbol(offendingSymbol: Any): SyntaxError {
         this.offendingSymbol = offendingSymbol
+        return this
     }
 
-    fun withLine(line: Int) {
+    fun withLine(line: Int): SyntaxError {
         this.line = line
+        return this
     }
 
-    fun withCharPositionInLine(charPositionInLine: Int) {
+    fun withCharPositionInLine(charPositionInLine: Int): SyntaxError {
         this.charPositionInLine = charPositionInLine
+        return this
     }
 
-    fun withMessage(msg: String) {
+    fun withMessage(msg: String): SyntaxError {
         this.msg = msg
+        return this
     }
 
-    fun withRecognitionException(e: RecognitionException) {
+    fun withRecognitionException(e: RecognitionException): SyntaxError {
         this.e = e
+        return this
     }
 
     fun verify() = listener.syntaxError(recognizer, offendingSymbol, line, charPositionInLine, msg, e)
