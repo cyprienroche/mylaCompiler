4e943b4a21afd46d4617a1d8a8b222ce29f726d6
Cyprien <roche.cyprien@gmail.com>
2020-03-17 13:17:49 +0100

    refactor Main and move other fun to utils

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,4 @@
-import frontend.ParseTreeGen
-import frontend.errors.ErrorListener
-import frontend.errors.SemanticError
-import frontend.errors.SemanticErrorException
-import frontend.errors.SyntaxError
 import frontend.errors.SyntaxErrorException
-import frontend.errors.SyntaxErrorListener
-import java.io.File
 import kotlin.system.exitProcess
 
 fun main() {
@@ -18,27 +11,4 @@ fun main() {
         println(e.message())
         exitProcess(e.errorCode)
     }
-}
-
-private fun isValidFile(fileName: String): Boolean {
-    val input = File(fileName)
-
-    if (!input.isFile || input.extension != "myla") {
-        println("Error: the file ${input.name} is not a valid .myla file.")
-        return false
-    }
-
-    return true
-}
-
-fun generateAst(fileName: String) {
-    val syntaxListener = ErrorListener<SyntaxError>()
-    val tree = ParseTreeGen(fileName, SyntaxErrorListener(syntaxListener)).parseTree()
-
-    if (syntaxListener.hasErrors) throw SyntaxErrorException(syntaxListener.errors)
-    val semanticsListener = ErrorListener<SemanticError>()
-    if (syntaxListener.hasErrors) throw SyntaxErrorException(syntaxListener.errors)
-    if (semanticsListener.hasErrors) throw SemanticErrorException(semanticsListener.errors)
-
-    println(tree.toStringTree())
 }
