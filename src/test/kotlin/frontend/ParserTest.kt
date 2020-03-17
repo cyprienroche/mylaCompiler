cb8a64987c763adefdee8599108b71179815a929
Cyprien <roche.cyprien@gmail.com>
2020-03-17 19:21:45 +0100

    [error] ParserTest use ValueSource instead of Csv which was cutting strings containing '\n'

--- src/test/kotlin/frontend/ParserTest.kt
+++ src/test/kotlin/frontend/ParserTest.kt
@@ -1,87 +1,76 @@
 package frontend
 
-import com.nhaarman.mockitokotlin2.atLeastOnce
-import com.nhaarman.mockitokotlin2.never
 import frontend.mock.MockParser
-import frontend.mock.SyntaxError
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
 
 class ParserTest {
 
     internal class LiteralTest {
 
         @ParameterizedTest
-        @CsvSource("1", "-2", "+123")
-        internal fun validInteger(input: String) {
+        @ValueSource(strings = ["1", "-2", "+123"])
+        fun validInteger(input: String) {
             val mock = MockParser(input)
-            mock.parseLiteral()
-            SyntaxError(mock.listener, never()).verify()
+            mock.parseLiteral().verifyValid()
         }
 
         @ParameterizedTest
-        @CsvSource("(", "-1i", "c3p0", "++3")
-        internal fun invalidInteger(input: String) {
+        @ValueSource(strings = ["(", "-1i", "c3p0", "++3"])
+        fun invalidInteger(input: String) {
             val mock = MockParser(input)
-            mock.parseLiteral()
-            SyntaxError(mock.listener, atLeastOnce()).verify()
+            mock.parseLiteral().verifyInvalid()
         }
     }
 
     internal class ExpressionTest {
 
         @ParameterizedTest
-        @CsvSource("1 + 1", "(2--3)", "1 * (3 + 2)", "+2-3*4/(+33+ \r - -3 + \t 10 ++5)")
-        internal fun validExpression(input: String) {
+        @ValueSource(strings = ["1 + \n 1", "(2--3)", "1 * (3 + 2)", "+2-3*4/(+33+ \r - -3 + \t 10 ++5)"])
+        fun validExpression(input: String) {
             val mock = MockParser(input)
-            mock.parseExpression()
-            SyntaxError(mock.listener, never()).verify()
+            mock.parseExpression().verifyValid()
         }
 
         @ParameterizedTest
-        @CsvSource("1 (2)", "x = 1", "--+-10", "6+*4", "0invalid")
-        internal fun invalidExpression(input: String) {
+        @ValueSource(strings = ["1 (2)", "x = 1", "+-10", "6+*4", "0invalid", "0 + "])
+        fun invalidExpression(input: String) {
             val mock = MockParser(input)
-            mock.parseExpression()
-            SyntaxError(mock.listener, atLeastOnce()).verify()
+            mock.parseExpression().verifyInvalid()
         }
     }
 
     internal class AssignTest {
 
         @ParameterizedTest
-        @CsvSource("identifier", "Variable", "_value____0")
-        internal fun validLHSAssignment(input: String) {
+        @ValueSource(strings = ["identifier", "Variable", "_value____0"])
+        fun validLHSAssignment(input: String) {
             val mock = MockParser(input)
-            mock.parseAssignLHS()
-            SyntaxError(mock.listener, never()).verify()
+            mock.parseAssignLHS().verifyValid()
         }
 
         @ParameterizedTest
-        @CsvSource("1ident", "=var", "()", "+i")
-        internal fun invalidLHSAssignment(input: String) {
+        @ValueSource(strings = ["1ident", "=var", "()", "+i"])
+        fun invalidLHSAssignment(input: String) {
             val mock = MockParser(input)
-            mock.parseAssignLHS()
-            SyntaxError(mock.listener, atLeastOnce()).verify()
+            mock.parseAssignLHS().verifyInvalid()
         }
     }
 
     internal class StatementTest {
 
         @ParameterizedTest
-        @CsvSource("x = 2", "y = -z + \t 3", "if =  _value____0")
-        internal fun validStatement(input: String) {
+        @ValueSource(strings = ["x = \n 2", "y = -z + \n \t 3", "if =  _value____0"])
+        fun validStatement(input: String) {
             val mock = MockParser(input)
-            mock.parseStatement()
-            SyntaxError(mock.listener, never()).verify()
+            mock.parseStatement().verifyValid()
         }
 
         @ParameterizedTest
-        @CsvSource("10 = x", "+z = z", "=p = 0", "()")
-        internal fun invalidStatement(input: String) {
+        @ValueSource(strings = ["10 = x", "+z = z", "=p = 0", "()"])
+        fun invalidStatement(input: String) {
             val mock = MockParser(input)
-            mock.parseStatement()
-            SyntaxError(mock.listener, atLeastOnce()).verify()
+            mock.parseStatement().verifyInvalid()
         }
     }
 }
