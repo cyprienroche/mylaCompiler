b67e6417d97ab03b5d324add131b559545354714
Cyprien <roche.cyprien@gmail.com>
2020-03-19 15:56:17 +0100

    [ast] [VisitorTest][test] add assertion for visiting assignment program

--- src/test/kotlin/frontend/visitor/VisitorTest.kt
+++ src/test/kotlin/frontend/visitor/VisitorTest.kt
@@ -1,8 +1,11 @@
 package frontend.visitor
 
+import ast.ProgramTree
 import com.nhaarman.mockitokotlin2.mock
 import frontend.ParseTreeGenerator
 import org.antlr.v4.runtime.ANTLRErrorListener
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
 import org.junit.jupiter.api.Test
 import visitor.ProgramTreeVisitor
 
@@ -15,5 +18,7 @@ class VisitorTest {
     internal fun canVisitBasicProgram() {
         val parseTree = ParseTreeGenerator("$validProgramPath/assign.myla", listener).parseTree()
         val programTree = parseTree.accept(ProgramTreeVisitor())
+        val expectedTree = ProgramTree(emptyList())
+        assertThat(programTree, `is`(expectedTree))
     }
 }
