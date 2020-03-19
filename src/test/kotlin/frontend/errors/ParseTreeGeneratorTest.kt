c55ac6bfbe502ef8ba11d624a03f9a6a94928209
Cyprien <roche.cyprien@gmail.com>
2020-03-19 15:53:24 +0100

    [ast] [ParseTreeGeneratorTest][refactor] change name from SyntaxErrorTest to ParseTreeGeneratorTest

--- src/test/kotlin/frontend/errors/ParseTreeGeneratorTest.kt
+++ src/test/kotlin/frontend/errors/ParseTreeGeneratorTest.kt
@@ -3,19 +3,19 @@ package frontend.errors
 import com.nhaarman.mockitokotlin2.atLeastOnce
 import com.nhaarman.mockitokotlin2.mock
 import frontend.ParseTreeGenerator
-import frontend.mock.SyntaxError
+import frontend.mock.HasSyntaxError
 import org.antlr.v4.runtime.ANTLRErrorListener
 import org.junit.jupiter.api.Test
 
-class SyntaxErrorTest {
+class ParseTreeGeneratorTest {
 
     private val listener = mock<ANTLRErrorListener> {}
 
     @Test
-    fun assignBracketAddsErrorToListener() {
+    internal fun assignBracketAddsErrorToListener() {
         val parseTree = ParseTreeGenerator("src/test/resources/invalid/assignBracket.myla", listener)
         parseTree.parseTree()
-        SyntaxError(listener, atLeastOnce())
+        HasSyntaxError(listener, atLeastOnce())
             .withLine(1)
             .withCharPositionInLine(4)
             .withMessage("token recognition error at: '{'")
@@ -23,10 +23,10 @@ class SyntaxErrorTest {
     }
 
     @Test
-    fun assignBinOpAddsErrorToListener() {
+    internal fun assignBinOpAddsErrorToListener() {
         val parseTree = ParseTreeGenerator("src/test/resources/invalid/assignBinOp.myla", listener)
         parseTree.parseTree()
-        SyntaxError(listener, atLeastOnce())
+        HasSyntaxError(listener, atLeastOnce())
             .withLine(1)
             .withCharPositionInLine(7)
             .withMessage("no viable alternative at input 'x=5+'")
@@ -34,10 +34,10 @@ class SyntaxErrorTest {
     }
 
     @Test
-    fun integerDeclarationAddsErrorToListener() {
+    internal fun integerDeclarationAddsErrorToListener() {
         val parseTree = ParseTreeGenerator("src/test/resources/invalid/integerDeclaration.myla", listener)
         parseTree.parseTree()
-        SyntaxError(listener, atLeastOnce())
+        HasSyntaxError(listener, atLeastOnce())
             .withLine(1)
             .withCharPositionInLine(0)
             .withMessage("mismatched input '1' expecting IDENT")
