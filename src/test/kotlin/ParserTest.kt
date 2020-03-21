c2d04b9652c46c51d593098a0eea161f43c54d92
Cyprien <roche.cyprien@gmail.com>
2020-03-21 17:54:26 +0100

    [ast] [ParserTest][refactor] move ParserTest to kotlin package

--- src/test/kotlin/ParserTest.kt
+++ src/test/kotlin/ParserTest.kt
@@ -1,6 +1,4 @@
-package frontend
-
-import frontend.mock.MockParser
+import mock.MockParser
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
