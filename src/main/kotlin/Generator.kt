e042f5d829c2af34974a54f0bc271c4636e8f5c0
Cyprien <roche.cyprien@gmail.com>
2020-03-21 18:16:06 +0100

    [ast] [Generator][refactor] use stream instead of string to generate parser

--- src/main/kotlin/Generator.kt
+++ src/main/kotlin/Generator.kt
@@ -1,12 +1,12 @@
 import generated.MylaLexer
 import generated.MylaParser
 import org.antlr.v4.runtime.ANTLRErrorListener
+import org.antlr.v4.runtime.CharStream
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
 import org.antlr.v4.runtime.tree.ParseTree
 
-fun generateParser(path: String, listener: ANTLRErrorListener): MylaParser {
-    val stream = CharStreams.fromFileName(path)
+fun generateParser(stream: CharStream, listener: ANTLRErrorListener): MylaParser {
     val lexer = MylaLexer(stream)
     val tokens = CommonTokenStream(lexer)
     val parser = MylaParser(tokens)
@@ -24,4 +24,4 @@ fun generateParseTree(parser: MylaParser): ParseTree =
     parser.prog()
 
 fun generateParseTree(path: String, listener: ANTLRErrorListener): ParseTree =
-    generateParseTree(generateParser(path, listener))
+    generateParseTree(generateParser(CharStreams.fromFileName(path), listener))
