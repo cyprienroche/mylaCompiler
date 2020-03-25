93ac1ab8301f591c30be3670377b3c09717427c5
Cyprien <roche.cyprien@gmail.com>
2020-03-25 21:30:08 +0100

    [ast] [AssignmentTree][refactor] move AssignmentTree to its own class

--- src/main/kotlin/ast/StatementTree.kt
+++ src/main/kotlin/ast/StatementTree.kt
@@ -1,8 +1,3 @@
 package ast
 
-import ast.assignments.ExpressionTree
-import ast.assignments.VariableTree
-
 interface StatementTree
-
-data class AssignmentTree(val variable: VariableTree, val expression: ExpressionTree) : StatementTree
