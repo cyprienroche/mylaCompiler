3a96bab4c7f9df1a543bc212f71c18af6a00d7d8
Cyprien <roche.cyprien@gmail.com>
2020-03-25 21:42:07 +0100

    [ast] [StatementTreeVisitor][feature] make visitAssignStat use rhs and lhs when returning an AssignmentTree

--- src/main/kotlin/visitor/StatementTreeVisitor.kt
+++ src/main/kotlin/visitor/StatementTreeVisitor.kt
@@ -2,8 +2,6 @@ package visitor
 
 import ast.AssignmentTree
 import ast.StatementTree
-import ast.assignments.Arithmetic
-import ast.assignments.Identifier
 import generated.MylaParser
 import generated.MylaParserBaseVisitor
 
@@ -11,9 +9,9 @@ class StatementTreeVisitor : MylaParserBaseVisitor<List<StatementTree>>() {
 
     /* variable '=' expression */
     override fun visitAssignStat(ctx: MylaParser.AssignStatContext): List<StatementTree> {
-        // val lhs = ctx.variable().accept(VariableVisitor())
-        // val rhs = ctx.expression().accept(ExpressionVisitor())
-        return listOf(AssignmentTree(Identifier, Arithmetic))
+        val rhs = ctx.expression().accept(ExpressionTreeVisitor())
+        val lhs = ctx.variable().accept(VariableTreeVisitor())
+        return listOf(AssignmentTree(lhs, rhs))
     }
 
     /* stat ';' stat  */
